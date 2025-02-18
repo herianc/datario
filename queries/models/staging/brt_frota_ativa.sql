e0a1a27a96310f9492daff25403f6d67ae9aaf0a
Herian <herian.cavalcante@live.com>
2025-02-18 17:13:17 -0300

    feat: Alterando modelo DBT

--- queries/models/staging/brt_frota_ativa.sql
+++ queries/models/staging/brt_frota_ativa.sql
@@ -14,9 +14,8 @@ renamed AS (
         latitude AS lat,
         longitude AS lon,
         velocidade AS vel
-    FROM
-        source
-    WHERE (ignicao = true) AND (linha != '0')
+    FROM source
+    WHERE (ignicao = true) AND (linha != '0') AND (extraido_em >= NOW() - INTERVAL '7 day')
 )
 
 
